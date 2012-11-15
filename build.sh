d5618e13c7bf4bce96e4dee8aa631fd7df4a26ff
gboddaert <gboddaert@bestofmedia.com>
2012-11-15 16:17:45 +0100

    Fixed build

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ cd $DIR
 
 pear package-validate package.xml
 VERSION=$( xmllint --xpath "string(/*[name()='package']/*[name()='version']/*[name()='release'])" package.xml )
-tar -zcvf PHP_CodeSniffer_Standards_BestOfMedia-$VERSION.tgz package.xml PHP
+rm *.tgz
+tar -zcvf PHP_CodeSniffer_Standards_BestOfMedia-$VERSION.tgz package.xml CodeSniffer/*
