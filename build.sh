d84ffc77d4f9d5bfee6e1438f876ff19910d8788
Guibod <guibod@sf.net>
2012-11-18 23:27:39 +0100

    Polished 0.0.1 version

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ pear package-validate package.xml
 VERSION=$( xmllint --xpath "string(/*[name()='package']/*[name()='version']/*[name()='release'])" package.xml )
 
 mkdir -p PHP_CodeSniffer_Standards_BestOfMedia-$VERSION
-cp -R CodeSniffer PHP_CodeSniffer_Standards_BestOfMedia-$VERSION
+cp -R CodeSniffer LICENSE README PHP_CodeSniffer_Standards_BestOfMedia-$VERSION
 tar -zcvf PHP_CodeSniffer_Standards_BestOfMedia-$VERSION.tgz package.xml PHP_CodeSniffer_Standards_BestOfMedia-$VERSION
 rm -Rf PHP_CodeSniffer_Standards_BestOfMedia-$VERSION
